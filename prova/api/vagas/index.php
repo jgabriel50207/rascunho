9b3e20c6e946bd6da5db4daf9e71a90d06e3c808
emanuelricherd2003 <richardemanuel2003@gmail.com>
2025-06-05 11:35:45 -0300

    atualizacao estrutura

--- prova/api/vagas/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json");
-include_once("../db_conexao.php");
-
-$stmt = $pdo->query("SELECT * FROM vagas");
-$vagas = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($vagas);
-?>
